d11b1f70037f3d5ba7e1c8a8945f434ae6583c8f
admin <admin@MacBook-Pro-admin.local>
2024-03-02 13:22:26 +0300

    sending output on updates

--- 4.3.py
+++ 4.3.py
@@ -1,6 +1,9 @@
+import re
+#%%
 def countdown(n):
     print('start from: ',n)
     while n > 0:
         yield n
         n -=1
     print('thats all')    
+
--- apitest.py
+++ apitest.py
@@ -1,4 +1,27 @@
 import requests
-url='https://api.telegram.org/bot7008391655:AAHhlcoplGN6_qk0jzfpJwAU97kNQ3qAuNk/sendMessage?chat_id=1274684718&text=Hello eblo'
+import time
+api_url='https://api.telegram.org/bot'
+bot_token='7008391655:AAHhlcoplGN6_qk0jzfpJwAU97kNQ3qAuNk'
+text: str='you are eblo!'
+max_counter:int = 100
+offset:int=-2
+counter:int=0
+chat_id:int
+
+while counter<=max_counter:
+    print(f'number of messages= {counter}')
+    updates = requests.get(f'{api_url}{bot_token}/getUpdates?offset={offset + 1}').json()
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{api_url}{bot_token}/sendMessage?chat_id={chat_id}&text={text}')
+
+    time.sleep(1)
+    counter += 1
+
+
+
+
 response=requests.get(url)
 print(response.text)
